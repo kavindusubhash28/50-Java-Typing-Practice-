f9f86906778764483b820f75404dd65fb713f23b
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-07-18 22:13:29 +0530

    second 10questions done

--- Example.java
+++ Example.java
@@ -1,9 +1,9 @@
 class Example {
 	public static void main (String[] args) {
-		System.out.println("A");
-		System.out.println("B");
-		System.out.print();
-		System.out.println("C");
-		System.out.println("D");
+		int x=100,y,z=200;
+		System.out.println(x);
+		y="java";
+		System.out.println(y);
+		System.out.println(z);
 	}
 }
