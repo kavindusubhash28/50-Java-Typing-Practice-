9b30c8bb5c9de710dcce3ad90a6a56729e8c45d5
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-07-19 22:27:02 +0530

    upto 25th question completed

--- Example.java
+++ Example.java
@@ -1,9 +1,6 @@
 class Example {
 	public static void main (String[] args) {
-		int x=100,y,z=200;
-		System.out.println(x);
-		y="java";
-		System.out.println(y);
-		System.out.println(z);
+		System.out.println(Java);
+		System.out.println("Java");
 	}
 }
